c4c2664311f40e43a7053840b0b4c2dd08d46b48
benw <benw>
2012-07-03 06:52:30 +0000

    One less tracer (zooplankton chl)

--- verification/quota_1d/code/PTRACERS_SIZE.h
+++ verification/quota_1d/code/PTRACERS_SIZE.h
@@ -16,7 +16,7 @@ C  and is set here (default 1)
 C
 C     Number of tracers
       INTEGER PTRACERS_num
-      PARAMETER(PTRACERS_num = 75 )
+      PARAMETER(PTRACERS_num = 74 )
 
 
 #ifdef ALLOW_AUTODIFF_TAMC
